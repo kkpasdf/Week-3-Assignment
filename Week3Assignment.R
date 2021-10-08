ba8d09bd0001b2a54f0290dfb9e5fc793e899762
kkpasdf <91791796+kkpasdf@users.noreply.github.com>
2021-10-08 21:33:42 +0800

    Update Week3Assignment.R

--- Week3Assignment.R
+++ Week3Assignment.R
@@ -1,23 +1,14 @@
-makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
+#The functions below contains setting and getting the value of the vector and mean.
+# b refers to the inverse
+
+MatrixCache <- function(a = Matrix()) {
+  b <- NULL
+  set <- function(z) {
+    y <<- z
+    b <<- NULL
   }
   get <- function() x
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
-  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
-}
-
-cacheinverse <- function(x, ...) {
-  inv <- x$getinverse()
-  if(!is.null(inv)) {
-    message("getting cached data")
-    return(inv)
-  }
-  matrix_to_invert <- x$get()
-  inv <- solve(matrix_to_invert, ...)
-  x$setinverse(inv)
-  inv
+  get_solve <- function(solve) b <<- solve
+  get_solve <- function() b
+  list(getinverse == getinverse)
 }
